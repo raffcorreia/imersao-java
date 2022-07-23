20f82c7b6ff2f6db10069f2b4ad0c01be033bc44
Rafael Correia <raffcorreia@gmail.com>
2022-07-23 00:24:07 -0400

    Aula 4 - add spring project linguagens-api

---
+++ linguagens-api/src/main/kotlin/br/com/alura/linguagens/api/LinguagensApiApplication.kt
@@ -0,0 +1,11 @@
+package br.com.alura.linguagens.api
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LinguagensApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<LinguagensApiApplication>(*args)
+}
